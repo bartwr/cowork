a44727d79939c75ead33c4d3f449842d0d2c4093
Bart <bart@tuxion.nl>
2016-06-12 17:42:33 +0200

    update(cowork-landing) Made the 'form' nicer with clickable blobs :)

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ Awesome, why not add cowork.co as an app, so you will receive updates if we see 
 ## Privacy policy
 
 We don't use your email for anything other then logging you in*, so we can show you your personal dashboard. Read the [full privacy policy here](..).
+
+____
+
+Libs used
+
+- [ramda](http://ramdajs.com/) - functional programming
--- imports/ui/cowork-landing/cowork-landing.jsx
+++ imports/ui/cowork-landing/cowork-landing.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import R from 'ramda';
  
 export default class CoworkLanding extends Component {
  
@@ -8,31 +9,42 @@ export default class CoworkLanding extends Component {
   // +
   // http://leaverou.github.io/stretchy/?mc_cid=d0afee1116&mc_eid=5db727f525
 
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+
+    // getRandom :: Array -> Array node
+    this.getRandom = R.curry(function(arr){ return arr[ Math.floor( Math.random() * arr.length ) ] });
+
+    // somethingElse :: String -> String
+    this.styleClickableArea = R.curry(function(input){ return {__html: input} }); 
+
+    // renderClickableArea :: Array -> String
+    this.renderClickableArea = R.compose(this.styleClickableArea, this.getRandom)
+
   }
 
   render() {
 
-    var colors = {
-      iAm: '#00ff00',
-      workingOn: '#00f',
-      shareWithMe: '#f00'
-    }
-
     return (
       <form style={styles.base}>
         <span style={Object.assign({}, styles.infoBlock, {color: colors.iAm})}>
-          I am [a graphic designer / Simone],
+          I am <span style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.iAm})} dangerouslySetInnerHTML={this.renderClickableArea(['a graphic designer', 'Simone'])} />,
         </span>
         <span style={Object.assign({}, styles.infoBlock, {color: colors.workingOn})}>
-          I [work on a new, secret mobile application / am reading about quantum fysics]
+          I <span style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.workingOn})} dangerouslySetInnerHTML={this.renderClickableArea(['work on a new, secret mobile application', 'am reading about quantum fysics'])} />
         </span>
-        <span style={Object.assign({}, styles.infoBlock, {color: colors.shareWithMe})}>
-          &amp; I [like to get in contact with a web developer / am searching for a designer that can create a visual identity for X / am looking for a project manager / like to meet people who know a lot about block chain]
+        <span style={Object.assign({}, styles.infoBlock, {color: colors.lookingFor})}>
+          &amp; I <span style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.lookingFor})} dangerouslySetInnerHTML={this.renderClickableArea(['like to get in contact with a web developer', 'am searching for a designer that can create a visual identity for X', 'am looking for a project manager', 'like to meet people who know a lot about block chain'])} />
         </span>
       </form>
     );
   }
+}
+
+var colors = {
+  iAm: '#00ff00',
+  workingOn: '#00f',
+  lookingFor: '#f00' /*share with me */
 }
 
 var styles = {
@@ -44,5 +56,11 @@ var styles = {
   infoBlock: {
     display: 'inline-block',
     marginBottom: '25px'
+  },
+  clickableArea: {
+    display: 'inline',
+    color: '#fff',
+    borderRadius: '5px',
+    padding: '0 5px'
   }
 }
