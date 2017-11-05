bf2c0d7874f811201c6e39e6be5dbe33b704d81a
bartwr <mail@bartroorda.nl>
2017-11-05 22:51:30 +0100

    Form input placeholders are shown as `...` #animated. Fixes #4

--- imports/components/CoworkForm/CoworkForm.jsx
+++ imports/components/CoworkForm/CoworkForm.jsx
@@ -22,7 +22,11 @@ export default class CoworkLanding extends Component {
     super(props);
 
     // State
-    this.state = {};
+    this.state = {
+      iAmExamples: ['a graphic designer', 'Simone', 'Joost'],
+      workingOnExamples: ['work on a new, secret mobile application', 'am reading about quantum fysics'],
+      lookingForExamples: ['like to get in contact with a web developer', 'am searching for a designer that can create a visual identity for X', 'am looking for a project manager', 'like to meet people who know a lot about block chain']
+    };
 
     // getRandom :: Array -> Array node
     this.getRandom = R.curry(function(arr){ return arr[ Math.floor( Math.random() * arr.length ) ] });
@@ -33,11 +37,23 @@ export default class CoworkLanding extends Component {
     // renderClickableArea :: Array -> String
     this.renderClickableArea = R.compose(this.styleClickableArea, this.getRandom)
 
-  }
+    // Animate
+    self = this;
+    // Refresh random sentences x times
+    poke = (countTimes) => {
+      if(countTimes >= 0)
+        self.setState({
+          iAmExamples: ['...'],
+          workingOnExamples: ['...'],
+          lookingForExamples: ['...']
+        })
+      else {
+        self.forceUpdate();
+        setTimeout(() => poke(countTimes + 1), 1200);
+      }
+    }
+    setTimeout(() => poke(0), 1200);
 
-  handleChange(e) {
-    this.state[$(e.target).attr('name')] = $(e.target).val()
-    this.setState(this.state);
   }
 
   setValue = name => {
@@ -60,17 +76,17 @@ export default class CoworkLanding extends Component {
 
         <span style={Object.assign({}, styles.infoBlock, {color: colors.iAm})}>
           I am <span onClick={this.setValue.bind(this, 'iAm')} style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.iAm})}>{this.renderClickableArea(
-            this.state.iAm ? [this.state.iAm] : ['a graphic designer', 'Simone', '...']
+            this.state.iAm ? [this.state.iAm] : this.state.iAmExamples
           )}</span>,
         </span>
         <span style={Object.assign({}, styles.infoBlock, {color: colors.workingOn})}>
-          I <span onClick={this.setValue.bind(this, 'workingOn')} style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.workingOn})}>{this.renderClickableArea(
-            this.state.workingOn ? [this.state.workingOn] : ['work on a new, secret mobile application', 'am reading about quantum fysics', '...']
+          I work on <span onClick={this.setValue.bind(this, 'workingOn')} style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.workingOn})}>{this.renderClickableArea(
+            this.state.workingOn ? [this.state.workingOn] : this.state.workingOnExamples
           )}</span>
         </span>
         <span style={Object.assign({}, styles.infoBlock, {color: colors.lookingFor})}>
-          &amp; I <span onClick={this.setValue.bind(this, 'lookingFor')} style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.lookingFor})}>{this.renderClickableArea(
-            this.state.lookingFor ? [this.state.lookingFor] : ['like to get in contact with a web developer', 'am searching for a designer that can create a visual identity for X', 'am looking for a project manager', 'like to meet people who know a lot about block chain', '...']
+          &amp; I'm looking for <span onClick={this.setValue.bind(this, 'lookingFor')} style={Object.assign({}, styles.clickableArea, {backgroundColor: colors.lookingFor})}>{this.renderClickableArea(
+            this.state.lookingFor ? [this.state.lookingFor] : this.state.lookingForExamples
           )}</span>
         </span>
 
